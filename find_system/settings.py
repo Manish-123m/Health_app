12de383cacf46cac81e8198177a0c845c08f3ab0
Manish-123m <manishmaurya9616@gmail.com>
2025-02-22 19:44:11 +0530

    Updated ALLOWED_HOSTS for deployment

--- find_system/settings.py
+++ find_system/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-8z%!kd=so06&l)n00*#-!k4n@r))+xh0$5na^u08&$cpxx3r5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
